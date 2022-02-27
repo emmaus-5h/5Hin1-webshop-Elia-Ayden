f0172376094980e4d0ecd0a1808f98696b9ca6d1
Ayden0_0 <57942160+AIdan19@users.noreply.github.com>
2022-02-27 19:29:13 +0000

    geslacht in de api added

--- db/create.sql
+++ db/create.sql
@@ -8,6 +8,7 @@ CREATE TABLE products (
   name VARCHAR(255),
   price NUMERIC(10, 2),
   description TEXT,
+  geslacht TEXT,
   maten_id INTEGER
 );
 
@@ -26,20 +27,21 @@ CREATE TABLE maat (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-
-insert into products (name, price, description, code, maten_id) values ('custom 
-Unicorn x Rainbow', 60.00, 'om te dragen', '816905633-0', 1);
-insert into products (name,price, description, code, maten_id) values ('Jordan 1 high x Travis Scott ', 2238.00, 'The Travis Scott x Air Jordan 1 High OG is the limited edition collaboration from the rap superstar and Jordan Brand on the iconic silhouette that features a major twist.', '077030122-3', 2);
-insert into products (name, price, description, code, maten_id) values ('Off white vasity jacket', 1650.00, 'Off-White’s industrial themes collide with preppy Americana for AW20 to deliver this green varsity jacket. Made using a combination of virgin wool and white leather, it’s decorated with bold collegiate appliqués throughout, and completed with sporty striped ribbed knits.', '445924201-X', 3);
-insert into products (name, price, description, code, maten_id) values ('custom Angel jeans', 80.99, 'the custom Angel jeans is designed with our new V fit which is relaxed throughout and features a full color angel heat press artwork at the leg opening and finished with an unconstructed hem.', '693155505-7', 4);
-insert into products (name,price, description, code, maten_id) values ('custom Skeleton jeans',  80.99, 'the custom Skeleton jeans is designed with our new D fit which is relaxed throughout with an extended inseam for stacking and features sewn-on skeleton artwork in fleece on both legs and is constructed from light wash denim with a raw leg opening.', '686928463-6', 1);
-insert into products (name,price, description, code, maten_id) values (' Essential hoodie ' , 160.99, 'Essential Hoodie is a super soft, oversized hoodie that’s perfect for lounging around the house or wearing out and about.
-It’s made of our signature cotton-blend fabric with the right amount of stretch so you can move freely without restriction.', '492662523-7', 2);
-
 insert into maat (maten) values ('S');
 insert into maat (maten) values ('M');
 insert into maat (maten) values ('L');
 insert into maat (maten) values ('XL');
+
+insert into products (name, price, description, geslacht, code, maten_id) values ('custom 
+Unicorn x Rainbow', 60.00, 'om te dragen', 'Man & Vrouw', '816905633-0', 1);
+insert into products (name,price, description, geslacht, code, maten_id) values ('Jordan 1 high x Travis Scott ', 2238.00, 'The Travis Scott x Air Jordan 1 High OG is the limited edition collaboration from the rap superstar and Jordan Brand on the iconic silhouette that features a major twist.', 'Man', '077030122-3', 2);
+insert into products (name, price, description, geslacht, code, maten_id) values ('Off white vasity jacket', 1650.00, 'Off-White’s industrial themes collide with preppy Americana for AW20 to deliver this green varsity jacket. Made using a combination of virgin wool and white leather, it’s decorated with bold collegiate appliqués throughout, and completed with sporty striped ribbed knits.', 'Man', '445924201-X', 3);
+insert into products (name, price, description, geslacht, code, maten_id) values ('custom Angel jeans', 80.99, 'the custom Angel jeans is designed with our new V fit which is relaxed throughout and features a full color angel heat press artwork at the leg opening and finished with an unconstructed hem.', 'Man', '693155505-7', 4);
+insert into products (name,price, description, geslacht, code, maten_id) values ('custom Skeleton jeans',  80.99, 'the custom Skeleton jeans is designed with our new D fit which is relaxed throughout with an extended inseam for stacking and features sewn-on skeleton artwork in fleece on both legs and is constructed from light wash denim with a raw leg opening.','Man', '686928463-6', 1);
+insert into products (name,price, description, geslacht, code, maten_id) values (' Essential hoodie ' , 160.99, 'Essential Hoodie is a super soft, oversized hoodie that’s perfect for lounging around the house or wearing out and about.
+It’s made of our signature cotton-blend fabric with the right amount of stretch so you can move freely without restriction.','Man & Vrouw', '492662523-7', 2);
+
+
 
 
 /*

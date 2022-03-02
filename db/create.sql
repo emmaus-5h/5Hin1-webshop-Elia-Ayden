048fe20b8669cd49c141064b4c8637f3226987b1
Eliakim Koster <74650488+eliakimk@users.noreply.github.com>
2022-03-02 13:02:57 +0000

    Tabel veranderd en extra details

--- db/create.sql
+++ db/create.sql
@@ -38,8 +38,8 @@ insert into products (name,price, description, geslacht, code, maten_id) values 
 insert into products (name, price, description, geslacht, code, maten_id) values ('Off white vasity jacket', 1650.00, 'Off-White’s industrial themes collide with preppy Americana for AW20 to deliver this green varsity jacket. Made using a combination of virgin wool and white leather, it’s decorated with bold collegiate appliqués throughout, and completed with sporty striped ribbed knits.', 'Man', '445924201-X', 3);
 insert into products (name, price, description, geslacht, code, maten_id) values ('custom Angel jeans', 80.99, 'the custom Angel jeans is designed with our new V fit which is relaxed throughout and features a full color angel heat press artwork at the leg opening and finished with an unconstructed hem.', 'Man', '693155505-7', 4);
 insert into products (name,price, description, geslacht, code, maten_id) values ('custom Skeleton jeans',  80.99, 'the custom Skeleton jeans is designed with our new D fit which is relaxed throughout with an extended inseam for stacking and features sewn-on skeleton artwork in fleece on both legs and is constructed from light wash denim with a raw leg opening.','Man', '686928463-6', 1);
-insert into products (name,price, description, geslacht, code, maten_id) values (' Essential hoodie ' , 160.99, 'Essential Hoodie is a super soft, oversized hoodie that’s perfect for lounging around the house or wearing out and about.
-It’s made of our signature cotton-blend fabric with the right amount of stretch so you can move freely without restriction.','Man & Vrouw', '492662523-7', 2);
+insert into products (name,price, description, geslacht, code, maten_id) values (' Essential T shirt ' , 160.99, 'Essential T shirt is a super soft, oversized T that’s perfect for lounging around the house or wearing out and about.
+It’s made of our signature cotton-blend fabric with the right amount of stretch so you can move freely without restriction.','Man & Vrouw', '492662523-7', 2); 
 
 
 

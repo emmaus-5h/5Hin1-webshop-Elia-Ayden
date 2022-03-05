5e90e04c8e11a56598a9310a01c710e687b6ea5e
Eliakim Koster <74650488+eliakimk@users.noreply.github.com>
2022-03-05 23:05:47 +0000

    overzichtelijker gemaakt

--- db/create.sql
+++ db/create.sql
@@ -45,13 +45,14 @@ CREATE TABLE recycled (
 --
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
-
+-- TABLE maat
 insert into maat (maten) values ('S');
 insert into maat (maten) values ('M');
 insert into maat (maten) values ('L');
 insert into maat (maten) values ('XL');
 insert into maat (maten) values ('VEERTIG');  
-  
+
+-- TABLE products
 insert into products (name, price, description, geslacht, code, maten_id, categorie_id, materiaal_id, recycled_id) values ('custom 
 Unicorn x Rainbow', 60.00, 'this sweater is perfect for your children. the sweater is really comfortable and the material has a resistance against liquids, so if your child spills a drink it will bouns right off this amazing unicorn sweater!', 'Man & Vrouw', '816905633-0', 1, 1, 2, 2);
 insert into products (name, price, description, geslacht, code, maten_id, categorie_id, materiaal_id, recycled_id) values ('Jordan 1 high x Travis Scott ', 2238.00, 'The Travis Scott x Air Jordan 1 High OG is the limited edition collaboration from the rap superstar and Jordan Brand on the iconic silhouette that features a major twist.', 'Man', '077030122-3', 5, 2, 4, 1);
@@ -64,36 +65,16 @@ insert into products (name, price, description, geslacht, code, maten_id, catego
 insert into products (name, price, description, geslacht, code, maten_id, categorie_id, materiaal_id, recycled_id) values ('King Letterman jacket', 660.00, 'The King Letterman jacket is made from a premium wool blend with contrasting leather sleeves. The regular fit bomber jacket is fully lined on the inside', 'Man & Vrouw', '816905633-0', 2, 1, 1, 2);
 insert into products (name, price, description, geslacht, code, maten_id, categorie_id, materiaal_id, recycled_id) values ('Essential hoodie', 330.00, 'Essential hoodie is a super soft, oversized hoodie that’s perfect for lounging around the house or wearing out and about.', 'Man & Vrouw', '816905633-0', 3, 1, 1, 2);  
 
-insert into maat (maten) values ('S');
-insert into maat (maten) values ('M');
-insert into maat (maten) values ('L');
-insert into maat (maten) values ('XL');
-insert into maat (maten) values ('VEERTIG');  
-  
+-- TABLE categorie 
 insert into categorie (name) values ('top');
 insert into categorie (name) values ('bottom');
 
-
+-- TABLE materiaal
 insert into materiaal (name) values ('cotton');
 insert into materiaal (name) values ('nylon');
 insert into materiaal (name) values ('denim');
 insert into materiaal (name) values ('leather');
 
+  -- TABLE recycled
 insert into recycled (name) values ('yes');
 insert into recycled (name) values ('no');
-/*
-
-insert into information (size, model, material) values ('custom 
-Unicorn x Rainbow', 'om te dragen', '816905633-0');
-
-insert into information (size, model, material) values ('Jordan 1 high x Travis Scott ', 'The Travis Scott x Air Jordan 1 High OG is the limited edition collaboration from the rap superstar and Jordan Brand on the iconic silhouette that features a major twist.', '077030122-3');
-
-insert into  information(size, model, material) values ('Off white vasity jacket', 'Off-White’s industrial themes collide with preppy Americana for AW20 to deliver this green varsity jacket. Made using a combination of virgin wool and white leather, it’s decorated with bold collegiate appliqués throughout, and completed with sporty striped ribbed knits.', '445924201-X');
-
-insert into information(size, model, material) values ('custom Angel jeans', 'the custom Angel jeans is designed with our new V fit which is relaxed throughout and features a full color angel heat press artwork at the leg opening and finished with an unconstructed hem.', '693155505-7');
-
-insert into information (size, model, material) values ('custom Skeleton jeans', 'the custom Skeleton jeans is designed with our new D fit which is relaxed throughout with an extended inseam for stacking and features sewn-on skeleton artwork in fleece on both legs and is constructed from light wash denim with a raw leg opening.', '686928463-6');
-
-insert into information (size, model, material) values (' Essential hoodie ' , 'Essential Hoodie is a super soft, oversized hoodie that’s perfect for lounging around the house or wearing out and about.
-It’s made of our signature cotton-blend fabric with the right amount of stretch so you can move freely without restriction.', '492662523-7');
-*/
